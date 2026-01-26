c8392c882ec3c34e1187763af72ab054ef5dfecd
dzmbekovv7 <aziretdzumabekov19@gmail.com>
2026-01-26 15:10:56 +0600

    Magic8Ball project

---
+++ magic8ball/lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: BallPage(),
+    );
+  }
+}
+
+class BallPage extends StatelessWidget {
+  const BallPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text('Ask me anything'),
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({super.key});
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  void changeBallNumber() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+          child: TextButton(
+        onPressed: changeBallNumber,
+        child: Image(
+          image: AssetImage('images/ball$ballNumber.png'),
+        ),
+      )),
+    );
+  }
+}
